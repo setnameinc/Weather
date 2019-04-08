126a86e0693eb6bc90d653a2d3b6511302b69bb4
kolovsky <kartman561@gmail.com>
2019-04-08 19:38:50 +0300

    0.0.2 First usage of coroutines

--- app/src/main/java/com/setname/weather/mvp/utils/gestures/pinch/PinchToCloseCurrentCity.kt
+++ app/src/main/java/com/setname/weather/mvp/utils/gestures/pinch/PinchToCloseCurrentCity.kt
@@ -1,31 +1,43 @@
 package com.setname.weather.mvp.utils.gestures.pinch
 
-import android.util.Log
+import android.content.Context
+import android.util.DisplayMetrics
 import android.view.MotionEvent
 import android.view.View
 import android.view.ViewConfiguration
 
+
 class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
 
-    private var mPtrCount = 0
+    private val SCREEN_HEIGTH = 1920f;
 
-    private var mPrimStartTouchEventY = -1f
-    private var mSecStartTouchEventY = -1f
+    private var ptrCount = 0
 
-    private var mPrimSecStartTouchDistance = 0f
-    private var mViewScaledTouchSlop = 0
-    private var mPrevDistance = 0f
+    private var primaryStartTouchEventY = -1f
+    private var secondStartTouchEventY = -1f
 
-    private val mEndDistancePX = 200f
-    private val mEndSizePX: Float = 400f
+    private var primarySecondStartTouchDistance = 0f
+    private var viewScaledTouchSlop = 0
 
-    private var mScaleY = 0f
-    private var mMoveY = 0f
+    private var previousDistance = 0f
 
-    private val maxScale: Float = mEndDistancePX / 1920f
+    private val END_Y = 400f
+    private val END_SIZE: Float = 200f
+    private val PRIMARY_SECOND_DISTANCE = 200f
+
+    private val UP_ZONE = 100f
+    private val DOWN_ZONE = 1820f
+
+    private val INTERACTION_ZONE = DOWN_ZONE - UP_ZONE
+    private val INTERACTION_ZONE_FOR_PINCH = DOWN_ZONE - UP_ZONE - PRIMARY_SECOND_DISTANCE
+
+    private val END_SCALE: Float = END_SIZE / SCREEN_HEIGTH
+
+    private var scaleY = 0f
+    private var moveY = 0f
 
     init {
-        mViewScaledTouchSlop = viewConfig.scaledTouchSlop
+        viewScaledTouchSlop = viewConfig.scaledTouchSlop
     }
 
     fun onTouch(v: View, event: MotionEvent): Boolean {
@@ -36,65 +48,78 @@ class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
 
             MotionEvent.ACTION_POINTER_DOWN, MotionEvent.ACTION_DOWN -> {
 
-                mPtrCount++
-                if (mPtrCount == 2) {
+                ptrCount++
+                if (ptrCount == 2) {
 
-                    mPrevDistance = Float.MIN_VALUE
-                    mSecStartTouchEventY = event.getY(1)
-                    mPrimSecStartTouchDistance = distance(event, 0, 1)
+                    previousDistance = Float.MIN_VALUE
+                    secondStartTouchEventY = event.getY(1)
+                    primarySecondStartTouchDistance = distance(event, 0, 1)
 
-                        if (mPrimSecStartTouchDistance > 0) {
+                    if (primarySecondStartTouchDistance > 0) {
 
-                            mScaleY = 1 / (mPrimSecStartTouchDistance - mEndSizePX)
-                            mMoveY = ((960 - (mEndDistancePX /*- mEndSizePX*/)) / mPrimSecStartTouchDistance) - 0.01f
+                        scaleY = -(SCREEN_HEIGTH - END_SIZE) / (SCREEN_HEIGTH * INTERACTION_ZONE_FOR_PINCH)
+                        moveY = -(SCREEN_HEIGTH / 2 - UP_ZONE - END_Y) / INTERACTION_ZONE_FOR_PINCH
 
-                        } else {
+                    } else {
 
-                            mScaleY = 1 / -(mPrimSecStartTouchDistance + mEndSizePX)
-                            mMoveY = ((960 - (mEndDistancePX /*- mEndSizePX*/)) / -mPrimSecStartTouchDistance) - 0.01f
+                        scaleY = (SCREEN_HEIGTH - END_SIZE) / (SCREEN_HEIGTH * INTERACTION_ZONE_FOR_PINCH)
+                        moveY = (SCREEN_HEIGTH / 2 - UP_ZONE - END_Y) / INTERACTION_ZONE_FOR_PINCH
 
-                        }
+                    }
 
                 }
             }
 
             MotionEvent.ACTION_POINTER_UP, MotionEvent.ACTION_UP -> {
-                mPtrCount--
-                if (mPtrCount < 2) {
-                    mSecStartTouchEventY = -1f
+                ptrCount--
+                if (ptrCount < 2) {
+                    secondStartTouchEventY = -1f
+                    if (v.scaleY > 0.4) {
+                        v.animate()
+                            .scaleY(1f)
+                            .translationY(0f)
+                            .start()
+                    } else {
+                        v.animate()
+                            .scaleY(END_SCALE)
+                            .translationY(-465f)//TODO(calculating)
+                            .start()
+                    }
                 }
-                if (mPtrCount < 1) {
-                    mPrimStartTouchEventY = -1f
+                if (ptrCount < 1) {
+                    primaryStartTouchEventY = -1f
                 }
             }
 
             MotionEvent.ACTION_MOVE -> {
 
-                val isPrimMoving = isScrollGesture(event, 0, mPrimStartTouchEventY)
-                val isSecMoving = mPtrCount > 1 && isScrollGesture(event, 1, mSecStartTouchEventY)
+                val isPrimMoving = isScrollGesture(event, 0, primaryStartTouchEventY)
+                val isSecMoving = ptrCount > 1 && isScrollGesture(event, 1, secondStartTouchEventY)
 
                 if (isSecMoving) {
 
                     if (isPrimMoving && isSecMoving) {
 
-                        if (Math.abs(mPrimSecStartTouchDistance)>1720) {
+                        if (Math.abs(primarySecondStartTouchDistance) > INTERACTION_ZONE) {
 
                             val localDistance = distance(event, 0, 1)
 
-                            if (mPrevDistance != Float.MIN_VALUE) {
+                            if (Math.abs(localDistance) < INTERACTION_ZONE) {
 
-                                if (v.scaleY > maxScale) {
+                                if (v.scaleY > END_SCALE) {
 
-                                    scaleView(v, localDistance, mScaleY)
+                                    if (previousDistance != Float.MIN_VALUE) {
+
+                                        scaleView(v, localDistance, scaleY)
+                                        moveView(v, localDistance, moveY)
+
+                                    }
+
+                                    previousDistance = localDistance
 
                                 }
 
-                                moveView(v, localDistance, mMoveY)
-
-
                             }
-
-                            mPrevDistance = localDistance
 
                         }
 
@@ -114,11 +139,11 @@ class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
 
             if (distance < 0) {
 
-                return v.scaleY - (distance - mPrevDistance) * mScale
+                return v.scaleY - (distance - previousDistance) * mScale
 
             } else {
 
-                return v.scaleY + (distance - mPrevDistance) * mScale
+                return v.scaleY + (distance - previousDistance) * mScale
 
             }
 
@@ -127,19 +152,17 @@ class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
 
         v.scaleY = calculateScale(distance)
 
-
     }
 
     private fun moveView(v: View, distance: Float, mMoveScale: Float) {
 
-
         if (distance < 0) {
 
-            v.translationY -= (distance - mPrevDistance) * mMoveScale
+            v.translationY -= (distance - previousDistance) * mMoveScale
 
         } else {
 
-            v.translationY += (distance - mPrevDistance) * mMoveScale
+            v.translationY += (distance - previousDistance) * mMoveScale
 
         }
 
@@ -150,7 +173,7 @@ class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
 
         val moveY = Math.abs(event.getY(ptrIndex) - originalY)
 
-        return moveY > mViewScaledTouchSlop
+        return moveY > viewScaledTouchSlop
 
     }
 
@@ -165,6 +188,10 @@ class PinchToCloseCurrentCity(val viewConfig: ViewConfiguration) {
             return 0f
 
         }
+    }
+
+    fun convertPixelsToDp(context: Context, px: Float): Float {
+        return px / (context.resources.displayMetrics.densityDpi.toFloat() / DisplayMetrics.DENSITY_DEFAULT)
     }
 
 }
