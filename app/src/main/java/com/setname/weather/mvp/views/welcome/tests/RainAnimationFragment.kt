a14783da8b92428465e646553053bed153a3c320
kolovsky <kartman561@gmail.com>
2019-04-24 23:59:57 +0300

    alpha rain - testing clever rain - add some bugs :)

--- app/src/main/java/com/setname/weather/mvp/views/welcome/tests/RainAnimationFragment.kt
+++ app/src/main/java/com/setname/weather/mvp/views/welcome/tests/RainAnimationFragment.kt
@@ -3,10 +3,7 @@ package com.setname.weather.mvp.views.welcome.tests
 import android.animation.ValueAnimator.INFINITE
 import android.animation.ValueAnimator.RESTART
 import android.content.Context
-import android.graphics.Canvas
-import android.graphics.Color
-import android.graphics.Paint
-import android.graphics.Path
+import android.graphics.*
 import android.os.Bundle
 import android.support.v4.app.Fragment
 import android.util.Log
@@ -16,14 +13,10 @@ import android.view.ViewGroup
 import android.view.animation.LinearInterpolator
 import android.view.animation.TranslateAnimation
 import android.widget.FrameLayout
-import android.widget.ImageView
 import android.widget.SeekBar
 import com.setname.weather.R
 import kotlinx.android.synthetic.main.rain_test.view.*
-import kotlinx.coroutines.CoroutineScope
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
-import kotlinx.coroutines.withContext
+import kotlinx.coroutines.*
 import kotlin.random.Random
 
 /*
@@ -33,6 +26,9 @@ This is a demo view
  */
 
 class RainAnimationFragment : Fragment() {
+
+    private val screenHeight = 1920
+    private val screenWidth = 1080
 
     private lateinit var viewRainAnimationFragment: View
 
@@ -58,134 +54,78 @@ class RainAnimationFragment : Fragment() {
 
         }
 
+        seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {
+
+            }
+
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+
+                rainAnimation.rotation = (progress - 90).toFloat()
+
+            }
+
+        })
+
         return viewRainAnimationFragment
 
     }
 
     inner class RainAnimation(context: Context) : FrameLayout(context) {
 
-        private val poolSize = 8
-
-        private val imageList: MutableList<ImageView> = mutableListOf()
-
-        private val dropListVertical: MutableList<Drop> = mutableListOf()
-        private val dropListHorizontal: MutableList<Drop> = mutableListOf()
-
         init {
 
-            val LISTS_SIZE = 20
+            val countOfLines = 20
 
-            for (i in 0..LISTS_SIZE - 1) {
+            val areaDistribution = (screenHeight / countOfLines)
 
-                dropListVertical.add(Drop(context))
-                dropListVertical[i].redraw(-30.toDouble())
-                addView(dropListVertical[i])
-                /*delay(Random.nextInt(500, 1000).toLong())*/
+            val correctValForRotationFromHeightToWeigh = (screenHeight - screenWidth) / 2
 
-            }
+            CoroutineScope(Dispatchers.Main).launch {
 
-            for (i in 0..LISTS_SIZE - 1) {
+                withContext(Dispatchers.Main) {
 
-                dropListHorizontal.add(Drop(context))
-                dropListHorizontal[i].visibility = View.INVISIBLE
-                dropListHorizontal[i].redraw(-30.toDouble())
-                addView(dropListHorizontal[i])
+                    for (i in 0..countOfLines) {
 
-                /*delay(Random.nextInt(500, 1000).toLong())*/
+                        val startXForGenerationRandomX = i * areaDistribution - correctValForRotationFromHeightToWeigh
+                        val endXForGenerationRandomX =
+                            (i + 1) * areaDistribution - correctValForRotationFromHeightToWeigh
 
-            }
+                        val random =
+                            Random.nextInt(
+                                startXForGenerationRandomX,
+                                endXForGenerationRandomX
+                            ).toFloat()
 
-            var countOfVertical: Int = 0
-            var lastPos: Int = 0
+                        Log.i("RainAF", "random = ${random}")
 
-            seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
 
-                override fun onStartTrackingTouch(seekBar: SeekBar?) {}
-
-                override fun onStopTrackingTouch(seekBar: SeekBar?) {
-
-                    CoroutineScope(Dispatchers.Main).launch {
-
-                        withContext(Dispatchers.Main) {
-
-                            countOfVertical = (seekBar!!.progress - 90) * LISTS_SIZE / 90
-
-                            Log.i("RainAF", "countOfVertical = ${countOfVertical}")
-                            Log.i("RainAF", "angle = ${seekBar!!.progress - 90}")
-
-                            if (countOfVertical > 0) {
-
-                                for (i in lastPos..lastPos + Math.abs(countOfVertical)) {
-
-                                    dropListVertical[i].visibility = View.GONE
-                                    dropListHorizontal[i].visibility = View.VISIBLE
-
-                                    if (lastPos + countOfVertical - 1 == i) {
-
-                                        lastPos = countOfVertical
-
-                                    }
-
-                                }
-
-                            } else {
-
-                                for (i in lastPos + Math.abs(countOfVertical)..lastPos) {
-
-                                    dropListVertical[i].visibility = View.VISIBLE
-                                    dropListHorizontal[i].visibility = View.GONE
-
-                                    if (lastPos + countOfVertical - 1 == i) {
-
-                                        lastPos = countOfVertical
-
-                                    }
-
-                                }
-
-                            }
-
-                            for (i in dropListVertical) {
-
-                                i.redraw((seekBar!!.progress - 90).toDouble())
-
-                                /*delay(Random.nextInt(500, 1000).toLong())*/
-
-                            }
-
-                            for (i in dropListHorizontal){
-
-                                i.redraw((seekBar!!.progress - 90).toDouble())
-
-                            }
-
-                        }
+                        val drop =
+                            Drop(context, random)
+                        addView(drop)
+                        delay(Random.nextInt(500, 1000).toLong())
 
                     }
 
                 }
 
-                override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {}
+            }
 
-            })
 
         }
 
     }
 
-    inner class Drop(context: Context) : View(context) {
-
-        var angle = -30.toDouble()
-        var fromX = 0f
-        var toX = 0f
-
-        val customHeight = 1920f
-        val customWidth = 1080f
-
-        var curX = 0f
+    inner class Drop(context: Context, val fromX: Float) : View(context) {
 
         val paint = Paint()
-        lateinit var path: Path
+        var path: Path = Path()
+
+        val lineSeparatorLength = /*Random.nextInt(4, 6)*/ 5 * 100.toFloat()
+        val lineLength = 30f
 
         init {
 
@@ -193,58 +133,43 @@ class RainAnimationFragment : Fragment() {
             paint.color = Color.BLACK
             paint.strokeWidth = 3f
 
+            drawSeparatedLine()
+            setAnimation()
+
         }
 
         private fun setAnimation() {
-            fromX = Random.nextInt(1350).toFloat()
-            toX = fromX + customHeight * Math.tan(Math.toRadians(angle)).toFloat()
 
+            drawSeparatedLine()
+
+            //TODO(probably 1 think for bug)
+            val sumOfOneLine = (lineSeparatorLength + lineLength) / 2
+
+            //TODO(probably 2 think for bug)
             val animation =
-                TranslateAnimation(fromX, toX, -1920f, 1920f)
+                TranslateAnimation(fromX, fromX, -sumOfOneLine, sumOfOneLine)
+
             animation.apply {
 
-                duration = 5000L
+                duration = 1000L
                 repeatMode = RESTART
                 repeatCount = INFINITE
                 interpolator = LinearInterpolator()
 
             }
+
             this.startAnimation(animation)
-        }
-
-        fun redraw(curAngle: Double) {
-
-            angle = curAngle
-            path = Path()
-
-            drawSeparatedLine()
-            setAnimation()
-
-
-            invalidate()
 
         }
+
 
         private fun drawSeparatedLine() {
 
-            val dropLength = 20
+            path.moveTo(0f, 0f)
+            path.lineTo(0f, screenHeight.toFloat() + lineSeparatorLength + lineLength)
 
-            val fromXLocal = 100f
-            val dX = dropLength * Math.sin(Math.toRadians(angle))
-            val toXLocal = fromXLocal + dX
+            paint.pathEffect = DashPathEffect(floatArrayOf(lineLength, lineSeparatorLength), 0f)
 
-            val dY = Math.sqrt(dropLength * dropLength - dX * dX)
-
-            path.moveTo(fromXLocal, 0f)
-            path.lineTo(toXLocal.toFloat(), 0 + dY.toFloat())
-            path.moveTo(fromXLocal, 400f)
-            path.lineTo(toXLocal.toFloat(), 400f + dY.toFloat())
-            path.moveTo(fromXLocal, 800f)
-            path.lineTo(toXLocal.toFloat(), 800f + dY.toFloat())
-            path.moveTo(fromXLocal, 1200f)
-            path.lineTo(toXLocal.toFloat(), 1200f + dY.toFloat())
-            path.moveTo(fromXLocal, 1600f)
-            path.lineTo(toXLocal.toFloat(), 1600f + dY.toFloat())
 
         }
 
